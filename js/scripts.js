b7e871bb5f3fdafa420b8dcc79886a8aa1c69b46
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-02 15:21:33 -0600

    Changed url to https

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 var callapi = function() {
-    $.get("http://ec2-35-162-69-135.us-west-2.compute.amazonaws.com:8888/api/")
+    $.get("https://ec2-35-162-69-135.us-west-2.compute.amazonaws.com:8888/api/")
         .done(function(res) {
             $("#response").append((new Date()) + " " + res).css({color: "green"});
         })

14ac4b474b08db3c2d40c12c95d7282add93c449
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-02 12:12:06 -0600

    Updated for testing api

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+});
+
+var callapi = function() {
+    $.get("http://ec2-35-162-69-135.us-west-2.compute.amazonaws.com:8888/api/")
+        .done(function(res) {
+            $("#response").append((new Date()) + " " + res).css({color: "green"});
+        })
+        .fail(function(res) {
+            $("#response").append((new Date()) + " failed").css({color: "red"});
+        })
+        .always(function() {
+            $("#response").append("</br>");
+        });
+
+}

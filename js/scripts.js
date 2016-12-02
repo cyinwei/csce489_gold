b31bc38f4a1c50faafb0a01163c95e277d5aca2e
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-02 16:15:16 -0600

    Updated ip and cleaned up styling for tests

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,11 @@ $(document).ready(function() {
 });
 
 var callapi = function() {
-    $.get("https://ec2-35-162-69-135.us-west-2.compute.amazonaws.com:8888/api/")
+    $.get("https://ec2-35-163-195-254.us-west-2.compute.amazonaws.com:8888/api/")
         .done(function(res) {
-            $("#response").append((new Date()) + " " + res).css({color: "green"});
+            $("#response").append("<p style='color:green;'>" + (new Date()) + " success</p>");
         })
         .fail(function(res) {
-            $("#response").append((new Date()) + " failed").css({color: "red"});
+            $("#response").append(("<p style='color:red;'>" + new Date()) + " failed</p>");
         })
-        .always(function() {
-            $("#response").append("</br>");
-        });
-
 }

7c819278fce7759d045d27dd2511cf9ba042aa31
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-03 15:04:25 -0600

    Added POST function

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'), # Routes all 'http://<website>/api/' requests to the index function in api/views.py
+    url(r'^analyze$', views.analyze, name='analyze'), # Routes all 'http://<website>/api/analyze' requests to the analyze function in api/views.py
 ]
--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
 
 # Create your views here.
 
@@ -8,3 +11,18 @@ def index(req):
              This is where we would do our classifier calculation and just return the answer for the website to display.
              Super simple, huh?'''
     return HttpResponse(content=res, status=200)
+
+@require_POST
+@csrf_exempt
+def analyze(req):
+    res = {}
+    res['comment'] = req.POST['comment']
+    res['subreddit'] = req.POST['subreddit']
+    res['datetime'] = req.POST['datetime']
+
+    '''
+    This is where we will analyze the posted data.
+    We will run it through our trained classifier and return our analysis.
+    '''
+
+    return HttpResponse(json.dumps(res), content_type="application/json")

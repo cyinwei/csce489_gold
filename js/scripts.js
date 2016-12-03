1366bff2f0bcb5dc4fb9df9f2e09cabc3e717f1f
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-03 16:33:35 -0600

    finished inputs for form and display response

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,29 @@ var callapi = function() {
 $( "form" ).on( "submit", function( event ) {
     event.preventDefault();
 
-    var data = $( this ).serialize()
+    var req = $( this ).serialize()
     var unixtime = Date.parse(new Date())/1000
 
-    data += ("&datetime=" + unixtime)
+    req += ("&datetime=" + unixtime)
 
-    console.log( data );
+    console.log( req );
 
-    callapi()
+    $.post("https://ec2-35-163-195-254.us-west-2.compute.amazonaws.com/api/analyze", data=req)
+        .done(function(res) {
+            $("#res").css({'color': 'green'});
+        })
+        .fail(function(res) {
+            $("#res").css({'color': 'red'});
+        })
+        .always(function(res) {
+            $("#res").css({"display": "block"})
+
+            $("#ressubreddit").html(res['subreddit']);
+            $("#resdatetime").html(new Date(res['datetime'] * 1000));
+            $("#rescomment").html(res['comment']);
+
+            $("#response").html(JSON.stringify(res))
+            console.log(res)
+        });
+
 });

adc4b0d5b8eaa5e4a510cad65c4ca9933668913d
Jeremy Moorman <jsmoorman@gt.rr.com>
2016-12-06 14:28:05 -0600

    finished AskReddit

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -8,26 +8,57 @@ import re
 import pickle
 import pandas as pd
 import time
-
+import sys
+from scripts import Munger as mg
 
 '''
 Global variable, such as classifiers that will not change.
-Will need to load all 3 for each subreddit (30 in total)
-Yikes!
 '''
 module_dir = os.path.dirname(__file__)  # get current directory
-file_path = os.path.join(module_dir, 'baz.txt')
+
+badwords = pd.read_csv(os.path.join(module_dir, 'etc/Terms-to-Block.csv'))['Word']
 
 # TODO: Update location of classifiers after they are complete
 with open(os.path.join(module_dir, "./classifiers/tfidf_vectorizer.pickle"),'rb') as f:
     tfloaded = pickle.load(f) # imports classifier from file
 
-with open(os.path.join(module_dir, "./classifiers/kmeans_clustering.pickle"),'rb') as f:
+with open(os.path.join(module_dir, "./classifiers/watson_clusterer.pickle"),'rb') as f:
+    watsonloaded = pickle.load(f) # imports classifier from file
+
+with open(os.path.join(module_dir, "./classifiers/body_clusterer.pickle"),'rb') as f:
     kmloaded = pickle.load(f) # imports classifier from file
 
-with open(os.path.join(module_dir, "./classifiers/forest_classifier.pickle"),'rb') as f:
-    frloaded = pickle.load(f) # imports classifier from file
+def askreddit(df):
+    with open(os.path.join(module_dir, "./classifiers/AskReddit_gilded.pickle"),'rb') as f:
+        gilded = pickle.load(f) # imports classifier from file
 
+    with open(os.path.join(module_dir, "./classifiers/AskReddit_score.pickle"),'rb') as f:
+        score = pickle.load(f) # imports classifier from file
+
+    score_features = [
+        'Word Count',
+        'body cluster',
+        'Watson Anger',
+        'Watson Disgust',
+        'Watson Fear',
+        'Watson Joy',
+        'Watson Sadness',
+        'Watson Openness',
+        'Watson Conscientiousness',
+        'Watson Extraversion',
+        'Watson Agreeableness',
+        'Watson Emotional Range'
+    ]
+
+    gilded_features = features = [
+        'Score Bracket',
+        'Contains MD',
+        'Time of Day',
+    ]
+
+    # Classify with forest classifier
+    df['Score Bracket'] = score.predict(df[score_features])[0]
+    return str(gilded.predict(df[gilded_features])[0])
 
 # Create your views here.
 
@@ -44,26 +75,35 @@ def analyze(req):
     res = {}
     res['comment'] = req.POST['comment']
     res['subreddit'] = req.POST['subreddit']
-    res['datetime'] = int(time.time())
-    res['wordcount'] = len(re.findall("\S+", req.POST['comment']))
 
     # Create datafram
-    # TODO: Remove hard coded data (such as 'Contains MD') and let munger infer it
-    df = pd.DataFrame.from_dict({'body': [res['comment']], 'created_utc': [int(time.time())], 'Word Count': [res['wordcount']], 'Time of Day': [1], 'Contains MD': True})
+    df = pd.DataFrame.from_dict({'body': [res['comment']], 'created_utc': [int(time.time())]})
 
     # Munge Data
-    # TODO: Need to import munger package after completion
-    # df = mg.munge_dataset(df)
-    # df['Time of Day'] = df['Time of Day'].map({'Afternoon': 1,'Evening': 2,'Night': 3,'Morning': 4})
+    try:
+        df = mg.munge_dataset(df, badwords, 'a9b99375-b559-4e7e-a033-25c8ae178c6e', 'RRSUytaQ3nMz')
+    except:
+        return HttpResponse(content="Unexpected error:" + str(sys.exc_info()[0]), content_type="application/json")
 
     # Create TFIDF vector from body
     tfidf = tfloaded.transform(df['body'])
 
     # Cluster from TFIDF and put into dataframe
-    df['cluster'] = kmloaded.predict(tfidf)
+    df['body cluster'] = kmloaded.predict(tfidf)
 
-    # Calssify with forest classifier
-    res['gilded?'] = str(frloaded.predict(df[['Contains MD', 'Word Count', 'Time of Day', 'cluster']])[0])
+    # Cluster Watson analysis and put into dataframe
+    watson = ['Watson Anger', 'Watson Disgust', 'Watson Fear',
+            'Watson Joy', 'Watson Sadness', 'Watson Analytical',
+            'Watson Confident', 'Watson Tenative', 'Watson Openness',
+            'Watson Conscientiousness', 'Watson Extraversion',
+            'Watson Agreeableness', 'Watson Emotional Range']
+    df['watson cluster'] = watsonloaded.predict(df[watson].values)
+
+    # Determine which classifiers to use
+    if req.POST['subreddit'] == 'AskReddit':
+        res['gilded?'] = askreddit(df)
+    else:
+        return HttpResponse('Not a valid subreddit')
 
     # Respond with our conclusion
     return HttpResponse(json.dumps(res), content_type="application/json")
